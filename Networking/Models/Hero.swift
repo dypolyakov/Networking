e7fa322c0c76fe87937e7c2e4eb04dae12cb0557
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-01-28 18:26:46 +0500

    Initial Commit

---
+++ Networking/Models/Hero.swift
@@ -0,0 +1,26 @@
+//
+//  Hero.swift
+//  Networking
+//
+//  Created by Dmitry Polyakov on 28.01.2023.
+//
+
+struct Result: Decodable {
+    let result: Data
+}
+
+struct Data: Decodable {
+    let data: Heroes
+}
+
+struct Heroes: Decodable {
+    let heroes: [Hero]
+}
+
+struct Hero: Decodable {
+    let id: Int
+    let name: String
+    let name_english_loc: String
+    let primary_attr: Int
+    let complexity: Int
+}
---
+++ Networking/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Dmitry Polyakov on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchHeroes()
+    }
+}
+
+// MARK: - Networking
+extension ViewController {
+    private func fetchHeroes() {
+        guard let url = URL(string: "https://www.dota2.com/datafeed/herolist?language=russian") else { return }
+        print(url)
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            print(data)
+            
+            do {
+                let heroes = try JSONDecoder().decode(Result.self, from: data)
+                print(heroes)
+            } catch let error {
+                print(error)
+            }
+        }.resume()
+    }
+}
+
